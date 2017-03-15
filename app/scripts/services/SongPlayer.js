f719e67fda7764d683012c1aa848021f6115d233
Archita Patel <architapatelis@gmail.com>
2017-03-14 17:12:24 -0700

    checkpoint7-assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,12 +1,22 @@
 // Create a Factory Service (SongPlayer) for Bloc Jams
-// The service returns SongPlayer object, making its properties and methods public to the rest of the application
+// Note: we'll get 'song' from the Album View when a user clicks the play button. ngRepeat used in album.html will dictate which song to pass into function
 
 (function () {
     function SongPlayer () {
+        /**
+        * @desc The service returns this object, making its properties and methods public to the rest of the application
+        * @type {Object}
+        */
         var SongPlayer = {};
         
+        
+        /**
+        * @desc currently playing song
+        * @type {Object}
+        */
         // when no song is playing
         var currentSong = null;
+        
         
         /**
         * @desc Buzz object audio file
@@ -14,13 +24,12 @@
         */
         var currentBuzzObject = null;
         
+        
         /**
-        * @function setSong
+        * @function setSong - private function
         * @desc Stops currently playing song and loads new audio file as currentBuzzObject
         * @param {Object} song
         */
-        
-        // private function within SongPlayer service
         var setSong = function (song) {
             // Stop the currently playing song, if there is one.
             if(currentBuzzObject) {
@@ -40,18 +49,31 @@
         };
         
         
-        // create public play() method
-        // we'll get 'song' from the Album View when a user clicks the play button. ngRepeat used in Album View template will dictate which song to pass into function
+        /**
+        * @function playSong - private function
+        * @desc call Buzz's own play method on the newly created Buzz object
+        * @param {Object} song
+        */
+        var playSong = function (song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
+        
+        /**
+        * @function SongPlayer.play - public method
+        * @desc play method used by controller to play a song
+        * @param {Object} song
+        */
+    
         SongPlayer.play = function(song) {
             // if the currently playing song is not equal to the song the user clicks on
             // null !== song clicked
             // or song playing !== song clicked
             if (currentSong !== song) {
-                // call setSong function
+                // call setSong and playSong function
                 setSong(song);
-                // call Buzz's own play method on the newly created Buzz object
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
                 
             } else if (currentSong === song) {
                 if(currentBuzzObject.isPaused()) {
@@ -60,8 +82,11 @@
             }
         };
         
-        // create public pause() method
-        // a song must already be playing, when user clicks pause this method is triggered
+        /**
+        * @function SongPlayer.pause - public method
+        * @desc pause method used by controller to pause a song. A song must already be playing, when user clicks pause this method is triggered
+        * @param {Object} song
+        */
         SongPlayer.pause = function (song) {
             currentBuzzObject.pause();
             song.playing = false;
